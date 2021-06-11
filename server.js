75c149cf38cbe16459a82523722a7307d418a5db
Suparn Kumar <Sk8969150057@gmail.com>
2021-06-11 23:48:28 +0530

    added

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());//middleware to enable express parsing the json data
 const connectDB = require('./config/db');
+const { METHODS } = require('http');
 connectDB();
 
 //cors
 
 const corsOptions = {
     //origin ko ham env mein rkhrhe as a string , yahan array mein receive krna hai toh split krenge
-    origin: process.env.ALLOWED_CLIENTS.split(',')//origin pe wo saare options dene hote hain jahan se cors allow karna hai, multiple clients agar use krrhe hain toh simply array use krna hai
+    //origin: process.env.ALLOWED_CLIENTS.split(','),//origin pe wo saare options dene hote hain jahan se cors allow karna hai, multiple clients agar use krrhe hain toh simply array use krna hai
+    origin: "*",
     //['http://localhost:3000', 'http://localhost:5000'], aisa receive hoga yahan
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
 }
 app.use(cors(corsOptions)); //cors is a middleware
 
